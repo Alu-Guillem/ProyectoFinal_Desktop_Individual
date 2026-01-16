21f91668b6cf88579b7e6deed0731e391125702f
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-16 17:59:23 +0100

    feat(commands): add RelayCommand for MVVM pattern support

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace PereMaria.GestorHotel.Commands;
+
+public class RelayCommand(Action<object> execute, Predicate<object> canExecute) : ICommand
+{
+    private readonly Action<object> _execute = execute;
+    private readonly Predicate<object>? _canExecute = canExecute;
+
+    public RelayCommand(Action<object> execute) : this(execute, null)
+    {
+    }
+
+    public bool CanExecute(object? parameter) =>
+        _canExecute?.Invoke(parameter) ?? true;
+
+
+    public void Execute(object? parameter) =>
+        _execute(parameter);
+
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+}
