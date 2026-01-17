c96a99661691ce45be3a8723be4ee19550cf0e8b
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-17 01:17:27 +0100

    refactor: actualiza paleta de colores, fuentes y estilos base (cosmic theme)

---
+++ Components/HeaderBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PereMaria.GestorHotel.Components;
+
+public partial class HeaderBar : UserControl
+{
+    public HeaderBar()
+    {
+        InitializeComponent();
+    }
+}
