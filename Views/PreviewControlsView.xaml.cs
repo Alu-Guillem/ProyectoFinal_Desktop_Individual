77ead88f0d5dbdc04f80347a03a464ad5a72c8c2
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-19 16:19:08 +0100

    feat: agrega vista de controles previos con estilos y componentes personalizados

---
+++ Views/PreviewControlsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Views
+{
+    public partial class PreviewControlsView : UserControl
+    {
+        public PreviewControlsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
